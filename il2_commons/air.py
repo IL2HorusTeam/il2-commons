b2bf35967f27cdc5d6f30f007c82aebd0d584e99
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-04-17 17:36:30 +0300

    Add waypoint types.

--- il2_commons/air.py
+++ il2_commons/air.py
@@ -13,3 +13,25 @@ class Formation(Values):
     vic = VerboseValueConstant('F6', _("vic"))
     finger_four = VerboseValueConstant('F7', _("finger four"))
     diamond = VerboseValueConstant('F8', _("diamond"))
+
+
+class WaypointType(Values):
+    takeoff_normal = VerboseValueConstant('TAKEOFF', _("takeoff (normal)"))
+    takeoff_pair = VerboseValueConstant('TAKEOFF_002', _("takeoff (pair)"))
+    takeoff_in_line = VerboseValueConstant('TAKEOFF_003', _("takeoff (in line)"))
+
+    taxiing = VerboseValueConstant('TAKEOFF_004', _("taxiing"))
+
+    normal_fly = VerboseValueConstant('NORMFLY', _("normal fly"))
+
+    patrol_triangle = VerboseValueConstant('NORMFLY_401', _("patrol (triangle)"))
+    patrol_square = VerboseValueConstant('NORMFLY_402', _("patrol (square)"))
+    patrol_pentagon = VerboseValueConstant('NORMFLY_403', _("patrol (pentagon)"))
+    patrol_hexagon = VerboseValueConstant('NORMFLY_404', _("patrol (hexagon)"))
+    patrol_random = VerboseValueConstant('NORMFLY_405', _("patrol (random)"))
+
+    landing_on_left = VerboseValueConstant('LANDING', _("landing (on left)"))
+    landing_on_right = VerboseValueConstant('LANDING_101', _("landing (on right)"))
+    landing_short_on_left = VerboseValueConstant('LANDING_102', _("landing (short on left)"))
+    landing_short_on_right = VerboseValueConstant('LANDING_103', _("landing (short on right)"))
+    landing_straight = VerboseValueConstant('LANDING_104', _("landing (straight)"))
