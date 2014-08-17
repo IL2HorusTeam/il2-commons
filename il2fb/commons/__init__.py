f996289f89b28b7e2f6d29256eff625434f19e12
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-08-17 14:27:23 +0300

    add types of units (#2)

--- il2fb/commons/__init__.py
+++ il2fb/commons/__init__.py
@@ -29,3 +29,16 @@ class Skills(Values):
     average = VerboseValueConstant(1, _("average"))
     veteran = VerboseValueConstant(2, _("veteran"))
     ace = VerboseValueConstant(3, _("ace"))
+
+
+class UnitTypes(Values):
+    airplane = VerboseValueConstant('planes', _("airplane"))
+    armor = VerboseValueConstant('armor', _("armor"))
+    artillery = VerboseValueConstant('artillery', _("artillery"))
+    balloon = VerboseValueConstant('aeronautics', _("balloon"))
+    light = VerboseValueConstant('lights', _("light"))
+    radio = VerboseValueConstant('radios', _("radio"))
+    ship = VerboseValueConstant('ships', _("ship"))
+    stationary = VerboseValueConstant('stationary', _("stationary"))
+    train = VerboseValueConstant('trains', _("train"))
+    vehicle = VerboseValueConstant('vehicles', _("vehicle"))
